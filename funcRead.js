c384125196643370d13ac91c275274aee0389716
qiwi173 <skorolev784@gmail.com>
2024-02-02 11:09:28 +0300

    funcRead - done

--- funcRead.js
+++ funcRead.js
@@ -1,5 +1,10 @@
+const fs = require('fs')
+
 function read() {
+   const text = fs.readFileSync('./puzzles.txt', 'utf-8')
+   return text
    /**
     * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
     */
  }
+ console.log(read())
