84441bd7dc36d904892ce2c5d76d42660b997c3f
ChingizGalimov <chingizwp88@gmail.com>
2024-02-02 16:01:30 +0300

    fucSloved done

--- funcSolved.js
+++ funcSolved.js
@@ -2,35 +2,58 @@ const { read } = require('./funcRead.js');
 
 const sudoku1 = read();
 const str = sudoku1.split('\r\n');
-const firstLine = str[0];
+const procArgv = process.argv[2];
+const firstLine = str[procArgv];
 
 function solve(str) {
-  const newStr = str.split('');
+  const newStr = firstLine.split('');
   const arr = [];
 
-  for (let i = 0; i < newStr.length; i += 9) {
-    arr.push(newStr.slice(i, i + 9));
-    // console.log(arr)
-  }
-  for (let i = 0; i < arr.length; i += 1) {
-    if (arr[i] !== '-') {
-      let numStr = '';
-      numStr += arr[i];
-      console.log(arr[i]);
-      console.log(numStr);
-    }
-  }
-  for (const el of arr) {
-    if (el === '-') {
-      const randNum = Math.floor(Math.random() * 8) + 9;
-      if (!numStr.includes(randNum)) {
-        el.replace('-', randNum);
-      }
-    }
+  //   let numStr = '';
+  //   for (let j=0; j<newStr.length; j+=1){
+  //     if (newStr[0][j] !== '-') {
+  //         numStr += newStr[0][j];
+  //     }
+  // }
+  // console.log(numStr)
+  // for (const el of arr[0]) {
+  //     if (el === '-') {
+  //       let randNum = (Math.floor(Math.random() * 8) ).toString();
+  //     //   if (!numStr.includes(randNum.toString())) {
+  //     //     el.replace('-', randNum);
+  //     //   }
+  //     el.replace('-', randNum);
+  //     console.log(randNum);
+  //     }
+  //   }
+  //   return arr;
+  // let result;
+  // for (let j = 0; j < newStr.length; j += 9) {
+  //   arr.push(newStr.slice(j, j + 9));
+  // for (let i=0; i<newStr.length; i++){
+  // if (newStr[i] === '-'){
+  const result = newStr.map((el) => (el === '-' ? el = random() : el));
+  console.log(result);
+  //  console.log(result)
+
+  // }
+  // }
+
+  // }
+
+  // for (let i = 0; i < result.length; i += 9) {
+  //   arr.push(result.slice(i, i + 9));
+  // }return arr
+  for (let i = 0; i < result.length; i += 9) {
+    arr.push(result.slice(i, i + 9));
   }
   return arr;
 }
-
+// return result
+// }
+function random() {
+  return (Math.floor(Math.random() * 8) + 1).toString();
+}
 console.table(solve(firstLine));
 
 module.exports = { solve };
